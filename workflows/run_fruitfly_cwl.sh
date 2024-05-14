e3b33af3817242db24bc1351c6f3332f8525dd23
Jake Fennick <jake.fennick@axleinfo.com>
2024-05-13 18:54:01 -1000

    convert CWL to WIC in phases

--- workflows/run_fruitfly_cwl.sh
+++ workflows/run_fruitfly_cwl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-cwltool --strict-memory-limit --cachedir cachedir --provenance provenance/fruitfly_cwl --write-summary output_fruitfly_cwl.json --skip-schemas --relax-path-checks --leave-outputs fruitfly.cwl fruitfly_cwl_inputs.yml
+cwltool --strict-memory-limit --cachedir cachedir --provenance provenance/fruitfly --write-summary output_fruitfly.json --skip-schemas --relax-path-checks --leave-outputs fruitfly.cwl fruitfly_inputs.yml
